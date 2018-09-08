e1f79ea79a3a3bdd00c294f72522e05952dcca31
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-09-08 00:28:49 -0300

    Create Library and Selection Reducers

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
 
-export default class App extends Component {
-  render() {
-    return (
-      <Provider store={createStore(reducers)}>
-        <View>
-          <Header headerText="Tech Stack" />
-        </View>
-      </Provider>
-    );
-  }
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText="Tech Stack" />
+      </View>
+    </Provider>
+  );
 }
+
+export default App;
